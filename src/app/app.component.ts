916bff7a28aec59ead4b0b7ec205960966866946
Maurice Yap <mauriceyap@hotmail.co.uk>
2017-06-07 15:39:24 +0100

    [npd] connect java microservice with angular 2 app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+    text: string[];
+
+    constructor(private http: Http) {}
+
+    ngOnInit() {
+        this.http.get("http://justforex.xyz:3020/userapi-backend/economicIndicator/interestRate?indicator=JPN&time=2007-12-03T10:15:30").toPromise()
+            .then(r => r.json()).then(r => this.text = r);
+    }
 }
